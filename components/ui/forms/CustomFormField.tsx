989a11da4fed44e9f8acd5cdd5288790ddf84cbb
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-23 13:28:17 +0600

    Created resuable form

---
+++ components/ui/forms/CustomFormField.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import React from 'react'
+import {
+      
+      FormControl,
+      FormDescription,
+      FormField,
+      FormItem,
+      FormLabel,
+      FormMessage,
+    } from "@/components/ui/form"
+    import { Input } from "@/components/ui/input"
+import { Control } from 'react-hook-form'
+import { FormFieldType } from './PatientForm'
+
+interface CustomProps{
+      control:Control<any>,
+      fieldType:FormFieldType,
+      name:string,
+      label?:string,
+      placeholder?:string,
+      iconSrc?:string,
+      iconAlt?:string,
+      disabled?:boolean,
+      dateFormat?:string,
+      showTimeSelect:boolean,
+      children?:React.ReactNode,
+      renderSkeleton?:(field:any)=>React.ReactNode
+
+
+
+}
+const RenderInput=({field,props}:{field:any; props:CustomProps})=>{
+      // switch case use is much efficient this type of case
+     switch (key) {
+      case value:
+            
+            break;
+     
+      default:
+            break;
+     }
+
+
+      
+}
+
+
+const CustomFormField = (props:CustomProps) => {
+      const {control,fieldType,name,label}=props;
+
+  return (
+      <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {/* <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>
+            This is your public display name.
+          </FormDescription>
+          <FormMessage /> */}
+
+          {fieldType!==FormFieldType.CHECKBOX && label &&
+          (
+           <FormLabel>{label}</FormLabel>
+          )}
+           
+           <RenderInput field={field} props={props}/>
+           <FormMessage className='shad-error'/>
+
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default CustomFormField
--- components/ui/forms/PatientForm.tsx
+++ components/ui/forms/PatientForm.tsx
@@ -1,8 +1,67 @@
-import React from 'react'
+"use client"
 
-const PatientForm = () => {
-  return (
-    <div>PatientForm</div>
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "../form"
+import CustomFormField from "./CustomFormField"
+
+
+export enum FormFieldType{
+
+      INPUT ='input',
+      TEXTAREA='textarea',
+      PHONE_INPUT='phoneInput',
+      CHECKBOX='checkBox',
+      DATE_PICKER='datePicker',
+      SELECT='select',
+      SKELETON='skeleton',
+
+}
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+const PatientForm=()=> {
+  // 1. Define your form.
+  const form= useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+  return(
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-7 flex-1">
+            <section className="mb-10 space-y-4">
+                  
+                   <h1 className="header"> Hello there</h1>
+                   <p className='text-dark-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, ut.</p>
+            </section>
+            <CustomFormField
+             fieldType={FormFieldType.INPUT}
+             control={form.control}
+             name='name'
+             label='Full name'
+             placeholder='harun'
+             iconSrc='/assets/icons/user.svg'
+             iconAlt='user'
+
+             />
+        
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
