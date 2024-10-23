cae17a496605bc13e3e99e6f0188eac955e69967
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-23 12:06:56 +0600

    Home page created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import {Plus_Jakarta_Sans} from 'next/font/google'
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ui/ui/theme-provider";
 
-import {cn} from '/@lib/utils'
+
 
 
 const fontSans=Plus_Jakarta_Sans({
@@ -24,12 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn>}
-      >
-        {children}
-      </body>
-    </html>
+    <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
+import PatientForm from "@/components/ui/forms/PatientForm";
+import { Button } from "@/components/ui/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+
+
+
 
 export default function Home() {
   return (
-    <div className="text-5xl">
-      Home
+    <div className="flex h-screen max-h-screen">
+      {/* OTP verification */}
+
+       <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image 
+          src='/assets/icons/logo-full.svg'
+           alt="full-icon" 
+           width={1000}
+           height={1000}
+           className="h-10 mb-12 w-fit"/>
+           <PatientForm/>
+           <div className="text-14-regular mt-20 flex justify-between">
+           <p className="justify-items-end text-dark-600 xl:text-left">
+           © 2024 MediZone
+           </p>
+           <Link href='/?admin=true' className="text-blue-400">
+           Admin</Link>
+         
+           </div>
+
+        </div>
+       
+       </section>
+       <Image src="/assets/images/onboarding-img.png"
+       alt='patient'
+       height={1000}
+       width={1000}
+       className="side-img max-w-[50%]"
+       />
+
+       
     </div>
+   
   );
 }
