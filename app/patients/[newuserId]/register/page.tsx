eb00000c2348c4cf070664368441f2b663dba978
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-25 21:18:25 +0600

    Radio group component created for gender selection

--- app/patients/[newuserId]/register/page.tsx
+++ app/patients/[newuserId]/register/page.tsx
@@ -1,24 +1,29 @@
 
 import RegisterForm from '@/components/ui/forms/RegisterForm';
+import { getUser } from '@/lib/actions/patient.action';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-const Register = () => {
+const Register = async ({params:{userId}}:SearchParamProps) => {
+
+  const user= await getUser(userId);
+
+
   return (
       <div className="flex h-screen max-h-screen">
-      {/* OTP verification */}
 
-       <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[500px]">
+
+       <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex flex-1 flex-col py-10 ">
           <Image 
-          src='/assets/images/logo.png'
+           src='/assets/images/logo.png'
            alt="full-icon" 
            width={100}
            height={100}
-           className="h-30"/>
-
-           <RegisterForm/>
+           className="h-40 w-fit mb-10"/>
+ 
+           <RegisterForm user={user}/>
            
            <div className="text-14-regular mt-20 flex justify-between">
            <p className="justify-items-end text-dark-600 xl:text-left">
@@ -32,11 +37,11 @@ const Register = () => {
         </div>
        
        </section>
-       <Image src="/assets/images/register_img.png"
+       <Image src="/assets/images/register-img.png"
        alt='register'
        height={1000}
        width={1000}
-       className="side-img max-w-[390px]"
+       className="side-img max-w-[590px]"
        />
 
        
--- components/ui/forms/RegisterForm.tsx
+++ components/ui/forms/RegisterForm.tsx
@@ -4,32 +4,34 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import { Form } from "../form"
+import { Form, FormControl } from "../form"
 import CustomFormField from "./CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/Validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.action"
+import { FormFieldType } from "./PatientForm"
+import { RadioGroup } from "../ui/radio-group"
 
 
 
-export enum FormFieldType{
+// export enum FormFieldType{
 
-      INPUT ='input',
-      TEXTAREA='textarea',
-      PHONE_INPUT='phoneInput',
-      CHECKBOX='checkBox',
-      DATE_PICKER='datePicker',
-      SELECT='select',
-      SKELETON='skeleton',
+//       INPUT ='input',
+//       TEXTAREA='textarea',
+//       PHONE_INPUT='phoneInput',
+//       CHECKBOX='checkBox',
+//       DATE_PICKER='datePicker',
+//       SELECT='select',
+//       SKELETON='skeleton',
 
-}
-
-
+// }
 
 
-const RegisterForm =()=> {
+
+
+const RegisterForm=({user}:{user:User})=> {
   const router=useRouter()
   // 1. Define your form.
   const [isLoading,setIsLoading]=useState(false)
@@ -37,9 +39,9 @@ const RegisterForm =()=> {
   const form= useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
       defaultValues: {
-        name: "Hridoy",
-        email:"harun@gmail.com",
-        phone:"+8801746083370"
+        name: "",
+        email:"",
+        phone:""
     },
   })
 
@@ -72,23 +74,28 @@ const RegisterForm =()=> {
 
   return(
       <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-7 flex-1">
-            <section className="mb-5 space-y-4">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+            <section className="space-y-4">
                   
-                   <h1 className="header"> Hello there</h1>
-                   <p className='text-dark-700'>Login to make a schedule to an Apointment </p>
+                   <h1 className="header">Welcome</h1>
+                   <p className='text-dark-700'>Let us know about yourself </p>
             </section>
+            <section className="space-y-6">
+                  <div className="mb-9 space-y-1">
+                    <h2 className=" sub-header text-dark-700">Personal Information</h2>
+                  </div>
+
             <CustomFormField
              fieldType={FormFieldType.INPUT}
              control={form.control}
              name='name'
-             label='Full name'
+             label="Full name"
              placeholder='harun'
              iconSrc='/assets/icons/user.svg'
              iconAlt='user'
              />
-             
-              <CustomFormField
+             <div className="flex gap-6 flex-col xl:flex-row">
+             <CustomFormField
              fieldType={FormFieldType.INPUT}
              control={form.control}
              name='email'
@@ -97,7 +104,7 @@ const RegisterForm =()=> {
              iconSrc='/assets/icons/email.svg'
              iconAlt='email'
              />
-             
+
               <CustomFormField
              fieldType={FormFieldType.PHONE_INPUT}
              control={form.control}
@@ -105,8 +112,57 @@ const RegisterForm =()=> {
              label='Phone Number'
              placeholder='+8801632705569'
              />
-        
-        
+             </div>  
+            </section>
+            {/* <section className="space-y-6">
+                  <h2 className=" sub-header text-dark-700">Medical Information</h2>
+            </section> */}
+
+
+             <div className="flex flex-col gap-5 xl:flex-row">
+             <CustomFormField
+              fieldType={FormFieldType.DATE_PICKER}
+              control={form.control}
+              name="birthDate"
+              label="Date of birth"
+            />
+              <CustomFormField
+              fieldType={FormFieldType.SKELETON}
+              control={form.control}
+              name='gender'
+              label="Gender"
+              renderSkeleton={(field)=>(
+                  <FormControl>
+                        <RadioGroup
+                        >
+
+                        </RadioGroup>
+                  </FormControl>
+              )}
+               />
+
+             </div>
+             <div className="flex flex-col gap-5 xl:flex-row">
+
+             </div>
+             <div className="flex flex-col gap-5 xl:flex-row">
+
+             </div>
+             <div className="flex flex-col gap-5 xl:flex-row">
+
+             </div>
+             <div className="flex flex-col gap-5 xl:flex-row">
+
+             </div>
+             <div className="flex flex-col gap-5 xl:flex-row">
+
+             </div>
+             <div className="flex flex-col gap-5 xl:flex-row">
+
+             </div>
+
+
+              
       <SubmitButton isLoading={isLoading}>Get started</SubmitButton>
       
       </form>
