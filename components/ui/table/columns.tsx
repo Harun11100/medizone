b4d08d4b7954db617e792780bd65f154a86bc198
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-10-29 01:09:39 +0600

    Shadcn UI dialouge modal added to form the actions

--- components/ui/table/columns.tsx
+++ components/ui/table/columns.tsx
@@ -1,16 +1,16 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../dropdown-menu"
 import { Button } from "../button"
 import { ArrowUpDown, MoreHorizontal } from "lucide-react"
 import StatusBadge from "../StatusBadge"
 import { formatDateTime } from "@/lib/utils"
 import { Doctors } from "@/constant"
 import Image from "next/image"
+import AppointmentModal from "../AppointmentModal"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+
+
 export type Appointments = {
   id: string
   amount: number
@@ -76,6 +76,15 @@ export const columns: ColumnDef<Payment>[] = [
 
      }
 },
+{
+  id:'actions',
+  header:()=><div className="pl-4"> Actions</div>,
+  cell:({row})=>(
+    <div className="flex gap-1">
+          <AppointmentModal />
+    </div>
+  )
+},
 
 
 
@@ -90,36 +99,35 @@ export const columns: ColumnDef<Payment>[] = [
 
 
 
-
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const payment = row.original
+  // {
+  //   id: "actions",
+  //   cell: ({ row }) => {
+  //     const payment = row.original
  
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
+  //     // return (
+  //     //   <DropdownMenu>
+  //     //     <DropdownMenuTrigger asChild>
+  //     //       <Button variant="ghost" className="h-8 w-8 p-0">
 
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator/>
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-    },
-  },
+  //     //         <span className="sr-only">Open menu</span>
+  //     //         <MoreHorizontal className="h-4 w-4" />
+  //     //       </Button>
+  //     //     </DropdownMenuTrigger>
+  //     //     <DropdownMenuContent align="end">
+  //     //       <DropdownMenuLabel>Actions</DropdownMenuLabel>
+  //     //       <DropdownMenuItem
+  //     //         onClick={() => navigator.clipboard.writeText(payment.id)}
+  //     //       >
+  //     //         Copy payment ID
+  //     //       </DropdownMenuItem>
+  //     //       <DropdownMenuSeparator/>
+  //     //       <DropdownMenuItem>View customer</DropdownMenuItem>
+  //     //       <DropdownMenuItem>View payment details</DropdownMenuItem>
+  //     //     </DropdownMenuContent>
+  //     //   </DropdownMenu>
+  //     // )
+  //   },
+  // },
   
   
 
